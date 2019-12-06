eaa38af1bc8b38d6a38569efa644ddaf08f99f08
noslaver <noam575@gmail.com>
2019-12-06 15:53:19 +0200

    working listener/connection tests

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 set -e
-cd "$(dirname "${BASE_SOURCE[0]}")/.."
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 function main {
     python -m virtualenv .env --prompt "[neuron] "
-    find .env -name site-packages -exec bash -c 'echo "../../../.." > {}/self.pth' \;
+    find .env -name site-packages -exec bash -c 'echo "../../../../" > {}/self.pth' \;
     .env/bin/pip install -U pip
     .env/bin/pip install -r requirements.txt
 }
