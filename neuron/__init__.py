b8415c811aac510a3f4eee8f41c3971b20be7795
noslaver <noam575@gmail.com>
2020-02-29 15:27:12 +0200

    remove obsolete thought code

--- neuron/__init__.py
+++ neuron/__init__.py
@@ -1,6 +1,4 @@
-from .thought import Thought
 from .web import run_webserver
 from .server import run_server
-from .client import upload_thought
 
 version = '0.1.0'
--- neuron/client.py
+++ neuron/client.py
@@ -1,24 +1,7 @@
-from .thought import Thought
 from .protocol import Config, Hello
 from .reader import Reader
 from .utils import Connection
 import datetime as dt
-
-
-# @click.command
-# @click.option('-h', '--host', default='127.0.0.1', help='neuron server URL')
-# @click.option('-p', '--port', default=8000, help='neuron server URL')
-# @click.argument('path')
-# def upload_sample(host, port, path):
-#     address = host, port
-#     sample = parse_sample(path)
-#     upload_thought(address, int(user), thought)
-def upload_thought(address, user_id, thought):
-    conn = Connection.connect(*address)
-    with conn:
-        thought = Thought(user_id, dt.datetime.now(), thought)
-        msg = thought.serialize()
-        conn.send(msg)
 
 
 def upload_snapshot(address, path):
