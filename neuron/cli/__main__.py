78228ad50bb082656e774b279e3733f4e10c8d0b
noslaver <noam575@gmail.com>
2020-05-16 20:35:53 +0300

    implement cli

--- neuron/cli/__main__.py
+++ neuron/cli/__main__.py
@@ -1,5 +1,22 @@
 import click
+import json
 import requests
+
+
+def print_result(data):
+    data = json.loads(data)
+    print(json.dumps(data, indent=4, sort_keys=True))
+
+
+def get(host, port, url):
+    if not url.startswith('/'):
+        url += '/'
+    url = f'http://{host}:{port}{url}'
+    response = requests.get(url)
+
+    data = json.loads(response.content)
+
+    print_result(response.content)
 
 
 @click.group()
@@ -11,7 +28,7 @@ def cli():
 @click.option('--host', '-h', help='server\'s IP', default='127.0.0.1')
 @click.option('--port', '-p', help='server\'s port', type=int, default=8000)
 def get_users(host, port):
-    pass
+    get(host, port, '/users')
 
 
 @cli.command()
@@ -19,7 +36,7 @@ def get_users(host, port):
 @click.option('--host', '-h', help='server\'s IP', default='127.0.0.1')
 @click.option('--port', '-p', help='server\'s port', type=int, default=8000)
 def get_user(user_id, host, port):
-    pass
+    get(host, port, f'/users/{user_id}')
 
 
 @cli.command()
@@ -27,7 +44,7 @@ def get_user(user_id, host, port):
 @click.option('--host', '-h', help='server\'s IP', default='127.0.0.1')
 @click.option('--port', '-p', help='server\'s port', type=int, default=8000)
 def get_snapshots(user_id, host, port):
-    pass
+    get(host, port, f'/users/{user_id}/snapshots')
 
 
 @cli.command()
@@ -36,19 +53,19 @@ def get_snapshots(user_id, host, port):
 @click.option('--host', '-h', help='server\'s IP', default='127.0.0.1')
 @click.option('--port', '-p', help='server\'s port', type=int, default=8000)
 def get_snapshot(user_id, snapshot_id, host, port):
-    pass
+    get(host, port, f'/users/{user_id}/snapshots/{snapshot_id}')
 
 
 @cli.command()
 @click.argument('user_id', type=int)
 @click.argument('snapshot_id', type=int)
-@click.argument('result_id', type=int)
+@click.argument('result_id')
 @click.option('--host', '-h', help='server\'s IP', default='127.0.0.1')
 @click.option('--port', '-p', help='server\'s port', type=int, default=8000)
 @click.option('--save', '-s', 'path', help='save result to path',
               type=click.File('wb'))
 def get_result(user_id, snapshot_id, result_id, path, host, port):
-    pass
+    get(host, port, f'/users/{user_id}/snapshots/{snapshot_id}/{result_id}')
 
 
 if __name__ == '__main__':
